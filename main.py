dca8cb487484d718ab908c3147fb4e108a2d78f5
FCM777 <franck777.fc@gmail.com>
2023-11-11 12:08:02 -0400

    Create main.py
    configuración

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Estado": "Funcionando"}
+
+@app.post("/")
+
+@app.put("/")
+
+@app.patch("/")
+
+@app.delete("/")
+    
